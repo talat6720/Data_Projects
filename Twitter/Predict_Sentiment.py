c0f1e845caf6bef3d92bc8914c3954f91c47c577
Talat Sayyed <42927629+talat6720@users.noreply.github.com>
2024-01-13 13:36:52 +0100

    Create Predict_Sentiment.py

---
+++ Twitter/Predict_Sentiment.py
@@ -0,0 +1,7 @@
+tweet = "the movie is good "
+
+# Call the function
+predicted_sentiment = predict_sentiment(tweet)
+
+# Print the result
+print(f"The sentiment of the tweet is: {predicted_sentiment}")
