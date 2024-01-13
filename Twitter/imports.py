0cbe006967955da22a2a0de4248632b8b5afc76b
Talat Sayyed <42927629+talat6720@users.noreply.github.com>
2024-01-13 13:41:41 +0100

    imports.py

--- Twitter/imports.py
+++ Twitter/imports.py
@@ -4,3 +4,10 @@ pip install transformers
 from transformers import pipeline
 from collections import Counter
 import wordcloud
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
