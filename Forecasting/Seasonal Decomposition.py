2cf2eb6dcdf75e2c57453ec6b5bcbb255b38076c
Talat Sayyed <42927629+talat6720@users.noreply.github.com>
2024-01-16 22:30:55 +0100

    Update Seasonal Decomposition.py

--- Forecasting/Seasonal Decomposition.py
+++ Forecasting/Seasonal Decomposition.py
@@ -2,7 +2,6 @@ import pandas as pd
 from statsmodels.tsa.seasonal import seasonal_decompose
 import matplotlib.pyplot as plt
 
-# Assuming 'df1' is the DataFrame that contains the time series data
 # and that 'DateTime' is the column with time stamps and 'Consumption' is the target variable.
 df1 = pd.read_csv('electricityConsumptionAndProductioction.csv')
 
@@ -11,10 +10,6 @@ df1['DateTime'] = pd.to_datetime(df1['DateTime'])
 df1.set_index('DateTime', inplace=True)
 
 # Perform seasonal decomposition
-# The model can be 'additive' or 'multiplicative', depending on the nature of the series.
-# An additive model is linear where changes over time are consistently made by the same amount.
-# A multiplicative model is nonlinear, such as when changes increase or decrease over time.
-
 # We will use an additive model for this example.
 decomposition = seasonal_decompose(df1['Consumption'], model='additive', period=24)  # Assuming hourly data with daily seasonality
 
